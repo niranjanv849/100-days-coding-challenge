569b101cf012b0851d738a0902da9c321f76b5e1
niranjanv849 <niranjanv849@gmail.com>
2022-03-14 22:29:43 +0530

    Day - 11 🚀

---
+++ Niranjan/Python/niranjan-11.py
@@ -0,0 +1,32 @@
+# // Valentine's Day is approaching and thus Chef wants to buy some chocolates for someone special.
+
+# // Chef has a total of X rupees and one chocolate costs Y rupees. What is the maximum number of chocolates Chef can buy?
+
+# // Input Format
+# // First line will contain T, the number of test cases. Then the test cases follow.
+# // Each test case contains a single line of input, two integers X,Y - the amount Chef has and the cost of one chocolate respectively.
+# // Output Format
+# // For each test case, output the maximum number of chocolates Chef can buy.
+
+# // Constraints
+# // 1≤T≤1000
+# // 1≤X,Y≤100
+# // Sample Input 1 
+# // 4
+# // 5 10
+# // 16 5
+# // 35 7
+# // 100 1
+# // Sample Output 1 
+# // 0
+# // 3
+# // 5
+# // 100
+# // Explanation
+# // Test case-1: Chef has 5 rupees but the cost of one chocolate is 10 rupees. Therefore Chef can not buy any chocolates.
+
+# // Test case-2: Chef has 16 rupees and the cost of one chocolate is 5 rupees. Therefore Chef can buy at max 3 chocolates since buying 4 chocolates would cost 20 rupees.
+
+# // Test case-3: Chef has 35 rupees and the cost of one chocolate is 7 rupees. Therefore Chef can buy at max 5 chocolates for 35 rupees.
+
+print("Day -11")
