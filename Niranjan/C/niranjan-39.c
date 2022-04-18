5c225ec285992961f9510fbe77f1d2b2ca057cee
niranjanv849 <niranjanv849@gmail.com>
2022-04-18 21:24:58 +0530

    Day - 39

---
+++ Niranjan/C/niranjan-39.c
@@ -0,0 +1,16 @@
+// Write a c program to reverse given number.
+#include<stdio.h>  
+ int main()    
+{    
+int n, reverse=0, rem;    
+printf("Enter a number: ");    
+  scanf("%d", &n);    
+  while(n!=0)    
+  {    
+     rem=n%10;    
+     reverse=reverse*10+rem;    
+     n/=10;    
+  }    
+  printf("Reversed Number: %d",reverse);    
+return 0;  
+}   
