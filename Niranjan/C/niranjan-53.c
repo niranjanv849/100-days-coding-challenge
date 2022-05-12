b98b511d132490b7bda2d8af980d64c23437868c
niranjanv849 <niranjanv849@gmail.com>
2022-05-12 12:22:21 +0530

    Day -53🚀

---
+++ Niranjan/C/niranjan-53.c
@@ -0,0 +1,35 @@
+// Sort an Array Elements in Descending Order Program in C
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+ int i,a[10],temp,j;
+ printf("Enter any 10 num in array : \n");
+ for(i=0;i<=10;i++)
+ {
+ scanf("%d",&a[i]);
+ }
+ printf("\n\nData before sorting  = ");
+ for(j=0;j<10;j++)
+ {
+ printf("  %d",a[j]);
+ }
+ for(i=0;i<=10;i++)
+ {
+ for(j=0;j<=10-i;j++)
+ {
+ if(a[j]>a[j+1])
+ {
+  temp=a[j];
+  a[j]=a[j+1];
+  a[j+1]=temp;
+ }
+}
+}
+ printf("\n\n\nData after sorting   : ");
+ for(j=9;j>=0 ;j--)
+ {
+  printf("  %d", a[j]);
+  }
+}
