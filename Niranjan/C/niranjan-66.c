0930b96a5a31d55fda93db9dedb39bf0ad8e4db6
niranjanv849 <niranjanv849@gmail.com>
2022-05-12 12:31:26 +0530

    Day -66🚀

---
+++ Niranjan/C/niranjan-66.c
@@ -0,0 +1,27 @@
+// Find Duplicate Elements in Array in C
+
+#include<stdio.h>
+#include<conio.h>
+
+ void main()
+ {
+  int i,arr[20],j,no;
+  printf("Enter size of array: ");
+  scanf("%d",&no);
+  printf("Enter any %d elements in array: ",no);
+  for(i=0;i<no;i++)
+  {
+   scanf("%d",&arr[i]);
+  }
+  printf("Duplicate elements are: ");
+  for(i=0; i<no; i++)
+   {
+    for(j=i+1;j<no;j++)
+    {
+    if(arr[i]==arr[j])
+    {
+    printf("%d\n",arr[i]);
+    }
+   }
+   }
+ }
