c67ef46b3dc43cbb10d60b92366806f5d4e430d5
niranjanv849 <niranjanv849@gmail.com>
2022-05-16 14:35:29 +0530

    Day - 74🚀

---
+++ Niranjan/C/niranjan-75.c
@@ -0,0 +1,19 @@
+// Print Number Pattern Program in C
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+int i,j, k=1;
+clrscr();
+for(i=1;i<=5;i++)
+{
+for(j=1;j<i;j++)
+{
+printf("%d",k);
+k++
+}
+printf("\n");
+}
+getch();
+}
