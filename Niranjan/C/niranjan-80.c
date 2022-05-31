9f93844fd8699c566c710cee31e8d6446f71539b
niranjanv849 <niranjanv849@gmail.com>
2022-05-31 13:58:26 +0530

    Day -80🚀

---
+++ Niranjan/C/niranjan-80.c
@@ -0,0 +1,33 @@
+// Write C Program to convert decimal number to binary using function
+#include <stdio.h>
+#include <math.h>
+ 
+//Function declaration
+long long convertDecimalToBinary(int n);
+ 
+int main()
+{
+    int n;
+    printf("Enter a decimal number: ");
+    // Inputting numbers from user
+    scanf("%d", &n);
+    printf("%d in decimal = %lld in binary", n, convertDecimalToBinary(n));
+    return 0;
+}
+ 
+long long convertDecimalToBinary(int n)
+{
+    long long binaryNumber = 0;
+    int remainder, i = 1, step = 1;
+ 
+    while (n!=0)
+    {
+        remainder = n%2;
+        //Printing decimal number to binary
+        printf("Step %d: %d/2, Remainder = %d, Quotient = %d\n", step++, n, remainder, n/2);
+        n /= 2;
+        binaryNumber += remainder*i;
+        i *= 10;
+    }
+    return binaryNumber;
+}
